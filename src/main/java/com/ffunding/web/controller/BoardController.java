c16688de37fed5e2740afb8db62975f16a304d9a
WonE37 <WonE37@DESKTOP-41GRKB5>
2022-03-14 15:41:00 +0900

    37

--- src/main/java/com/ffunding/web/controller/BoardController.java
+++ src/main/java/com/ffunding/web/controller/BoardController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -123,5 +124,5 @@ public class BoardController {
 
 		return "redirect:/board/list";
 	}
-
+	
 }
